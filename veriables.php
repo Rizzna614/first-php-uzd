4439c2ed9f32542ef55f0c1b7b20c1ff51c07c58
you@example.com <you@example.com>
2025-10-30 09:29:40 +0200

    3.uzd done

---
+++ veriables.php
@@ -0,0 +1,11 @@
+<?php
+
+$groupName = "PT2025";
+$size = 25;
+$isActive = false;
+//const EULER = 2.718;       // tas pats bet ar const (abi ir constantes itka)
+define ("EULER", 2.718);    //sita dara ar define (abi ir constantes itka)
+
+echo $groupName, ", ", $size, ", ", EULER;
+
+
